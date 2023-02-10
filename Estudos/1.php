986f46b032d6eddb491bf61554f48dcec9776a1b
Zhunixy <kauan8643@gmail.com>
2023-02-10 08:16:52 -0300

    novas habilidades

---
+++ Estudos/1.php
@@ -0,0 +1,29 @@
+<?php
+
+//OPERAÇÕES TERNÁRIAS 
+
+/* 
+    Menor : <
+    Maior: >
+    Igual: == 
+    Menor ou igual: <=
+    Maior ou igual: >=
+    Negação: !=
+*/
+
+//Ternário, usar somendo quando tem 2 elementos, caso ao contrário usar if e else
+
+$dia = 20; //Se for abaixou ou igual a 15, será verdadeiro (true)
+$quinzena = ($dia <= 15) ? "Primeira Quinzena" : "Segunda Quinzena";
+
+echo "O Dia é $dia, <br> E Pertence a $quinzena";
+
+?>
+
+<?php
+//ESTRUTURA DE DECISÃPO IF E ELSE
+
+$nome = "João";
+$idade = 19;
+
+if($nome = )
